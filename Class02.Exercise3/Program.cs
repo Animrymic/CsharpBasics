55e7a3b49f423dc810720b72cd7584e887ddc22f
Animrymic <trojanovikdragan@gmail.com>
2026-03-16 20:54:19 +0100

    Add files via upload

---
+++ Class02.Exercise3/Program.cs
@@ -0,0 +1,8 @@
+﻿int phoneCredit = 102;
+int messageCredit = 5;
+
+int smsCount = phoneCredit / messageCredit;
+int remainingCredits = phoneCredit % messageCredit;
+
+Console.WriteLine(" You can send " + smsCount + " SMS messages. ");
+Console.WriteLine(" Credits left: " + remainingCredits);
---
+++ Class02.Operators/Program.cs
@@ -0,0 +1,29 @@
+﻿
+double a = 10;
+double b = 202;
+
+double sum = a + b;
+double diff = a - b;
+var multiply = a * b;
+double division = a / b;
+
+var complexExpression = ((a + b) / (a - b)) * multiply;
+
+Console.WriteLine(complexExpression);
+Console.WriteLine(sum);
+Console.WriteLine(diff);
+Console.WriteLine(multiply);
+Console.WriteLine(division);
+
+var reminder = a % 7;
+Console.WriteLine(reminder);
+
+Console.WriteLine(++b);
+Console.WriteLine(b++);
+Console.WriteLine(b);
+
+
+
+
+
+
