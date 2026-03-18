1a09413ab3c12405df09a23c583db4f7145e5f41
Animrymic <trojanovikdragan@gmail.com>
2026-03-18 20:13:57 +0100

    Add files via upload

---
+++ Class03.LoopsAndArrays/Program.cs
@@ -0,0 +1,101 @@
+﻿//#region Loops - For
+//using System.Collections.Concurrent;
+
+//Console.WriteLine("For loop - first");
+//for (int i = 0; i < 10; i++)
+//{
+//    Console.WriteLine("Iteration no. " + i);
+//}
+
+//Console.ReadLine();
+
+//Console.WriteLine("For loop - second");
+//for (int i = 10; i > 0; i--)
+//{
+//    Console.WriteLine("Iteration no. " + i);
+//}
+
+//Console.ReadLine();
+
+//// Infinite for loop
+////Console.WriteLine("Infinite for loop");
+////for(; ; )
+////{
+////    Console.WriteLine("This will go on forever");
+////}
+
+
+//// Loops indefinitely unless a condition in the block is met
+
+////for(; ; )
+////{
+////    Console.WriteLine("This will go on forever until you enter X or x!");
+////    string input = Console.ReadLine();
+////    if (input == "X" || input == "x");
+////        break;
+////}
+
+
+//Console.WriteLine("Creating a droid army: ");
+//for (int i = 1; i < 10; i++)
+//{
+//    Console.WriteLine("Assembling Droid Batalion " + i);
+
+//    //Thread.Sleep(2000);
+
+//    if (i == 5)
+//    {
+//        Console.WriteLine("Problem in assembling the Droid batalion " + i);
+//        continue;
+//    }
+//    if (i == 7)
+//    {
+//        Console.WriteLine("Droid assembler broke down!");
+//        break;
+//    }
+//    Console.WriteLine("Droid Batalion " + i + " is operational ");
+//}
+
+
+
+
+
+
+//while (true)
+//{
+//    Console.WriteLine("This will go on forever until you enter X or x!");
+//    string input = Console.ReadLine();
+//    if (input == "X" || input == "x")
+//        break;
+//}
+
+//Console.ReadLine();
+
+
+//Console.WriteLine("Creating a droid army: ");
+//int droidCounter = 1; ;
+//while (droidCounter <= 10)
+//{
+//    Console.Writeline("Cloning Clone Batalion " + droidCounter);
+//    if (droidCounter == 5)
+//    {
+//        Console.WriteLine("Problem in cloning the Clone batalion " + droidCounter);
+//        droidCounter++;
+//        continue;
+//    }
+//    if (droidCounter == 7)
+//    {
+//        Console.WriteLine("Cloning machine broke down!");
+//        droidCounter++;
+//        break;
+//    }
+
+//#endregion
+
+
+
+
+
+
+
+
