f4fadaa042ea7ae95ef070a3abc3b51fcf94965f
Animrymic <trojanovikdragan@gmail.com>
2026-03-18 20:14:38 +0100

    Add files via upload

---
+++ Class03.Exercise1/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+
+int startNumber = 1;
+
+while (startNumber <= number)
+{
+    Console.WriteLine(startNumber);
+    startNumber++;
+}
+
+Console.Write("Enter a number: ");
+int secondExerciseNumber = int.Parse(Console.ReadLine());
+
+int i = secondExerciseNumber;
+
+while (i >= 1)
+{
+    Console.WriteLine(i);
+    i--;
+}
+
+Console.Write("Enter a number: ");
+int thirdNumber = int.Parse(Console.ReadLine());
+
+int n = 2;
+
+while (n <= thirdNumber)
+{
+    Console.WriteLine(n);
+    n += 2;
+}
+
+Console.Write("Enter a number: ");
+int fourthNumber = int.Parse(Console.ReadLine());
+
+int m = 1;
+
+while (m <= fourthNumber)
+{
+    Console.WriteLine(m);
+    m += 2;
+}
---
+++ Class03.Exercise2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+
+int firstNumber = 1;
+
+while (firstNumber <= number)
+{
+    if(firstNumber == 100)
+    {
+        Console.WriteLine("The limit is reached!");
+        break;
+    }
+
+    if (firstNumber % 3 != 0 && firstNumber % 7 != 0)
+    {
+        Console.WriteLine(firstNumber);
+    }
+    firstNumber++;
+}
